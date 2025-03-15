64c25d254b4bd88cb88cc41954fa7e104ddf2835
kirikk__ - <zloybanan01@gmail.com>
2025-03-15 20:44:50 +0100

    Обновление бота

--- bot.py
+++ bot.py
@@ -18,34 +18,30 @@ print("Бот запущен...")
 app.run_polling()
 
 
+import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-TOKEN = "8154752140:AAEAT4LkB_8Y3NCx9YLrVHOf60zwUTr9GdU"
+logging.basicConfig(level=logging.INFO)
 
+TOKEN = "ТВОЙ_ТОКЕН"
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
-@dp.message_handler(commands=['/start'])
+@dp.message_handler(commands=['start'])
 async def start_command(message: types.Message):
  await message.reply("Привет! Я твой бот.")
 
-@dp.message_handler(commands=['/help'])
+@dp.message_handler(commands=['help'])
 async def help_command(message: types.Message):
  await message.reply("Я просто повторяю твои сообщения. Попробуй!")
 
-@dp.message_handler()
-async def echo(message: types.Message):
- await message.reply(message.text)
-
-if __name__ == "__main__":
- executor.start_polling(dp)
-
-@dp.message_handler(commands=['/menu'])
+@dp.message_handler(commands=['menu'])
 async def menu(message: types.Message):
- keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
 keyboard.add(KeyboardButton("Кто ты?"), KeyboardButton("Помощь"))
+await message.reply("Выбери действие:", reply_markup=keyboard)
 
 @dp.message_handler(lambda message: message.text == "Кто ты?")
 async def about_bot(message: types.Message):
@@ -54,3 +50,10 @@ async def about_bot(message: types.Message):
 @dp.message_handler(lambda message: message.text == "Помощь")
 async def help_message(message: types.Message):
  await message.reply("Напиши мне что-нибудь, и я повторю!")
+
+@dp.message_handler()
+async def echo(message: types.Message):
+ await message.reply(message.text)
+
+if __name__ == "__main__":
+executor.start_polling(dp, skip_updates=True)
