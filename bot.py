b371d106d7103702a79922acc9cee4824519c351
kirikk__ - <zloybanan01@gmail.com>
2025-03-15 20:18:08 +0100

    Обновление бота

--- bot.py
+++ bot.py
@@ -47,7 +47,7 @@ if __name__ == "__main__":
 async def menu(message: types.Message):
  keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
 keyboard.add(KeyboardButton("Кто ты?"), KeyboardButton("Помощь"))
- await message.reply("Выбери действие:", reply_markup=keyboard)
+await message.reply("Выбери действие:", reply_markup=keyboard)
 
 @dp.message_handler(lambda message: message.text == "Кто ты?")
 async def about_bot(message: types.Message):
