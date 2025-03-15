6b040d316f553e3f13b4d48c520b49c74d5b098e
kirikk__ - <zloybanan01@gmail.com>
2025-03-15 20:59:21 +0100

    Обновление бота

--- bot.py
+++ bot.py
@@ -40,7 +40,7 @@ async def help_command(message: types.Message):
 @dp.message_handler(commands=['menu'])
 async def menu(message: types.Message):
  keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-keyboard.add(KeyboardButton("Кто ты?"), KeyboardButton("Помощь"))
+ keyboard.add(KeyboardButton("Кто ты?"), KeyboardButton("Помощь"))
  await message.reply("Выбери действие:", reply_markup=keyboard)
 
 @dp.message_handler(lambda message: message.text == "Кто ты?")
