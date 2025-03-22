74639b15d78a338fe6d21dc7596ad8b8e9e1ae0e
kirikk__ - <zloybanan01@gmail.com>
2025-03-22 17:03:11 +0100

    Initial commit

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
+from telegram.ext import Application, CommandHandler, ContextTypes
 import os
 import logging
 
@@ -8,25 +8,21 @@ logging.basicConfig(level=logging.DEBUG)
 TOKEN = os.getenv('8154752140:AAEAT4LkB_8Y3NCx9YLrVHOf60zwUTr9GdU')
 PORT = int(os.getenv('PORT', 8443))
 
-def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Привет! Я работаю на Render!')
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text('Привет! Я работаю на Render!')
 
-def main() -> None:
-    updater = Updater(TOKEN, use_context=True)
+async def main() -> None:
+    application = Application.builder().token(TOKEN).build()
 
-    dispatcher = updater.dispatcher
+    application.add_handler(CommandHandler("start", start))
 
-    dispatcher.add_handler(CommandHandler("start", start))
-
-    updater.start_webhook(
+    await application.run_webhook(
         listen="0.0.0.0",  
         port=PORT,         
         url_path=TOKEN,    
+        webhook_url=f"https://new-repository-101.onrender.com/{TOKEN}"  
     )
 
-    updater.bot.set_webhook(f"https://dashboard.render.com/web/srv-cvarb32n91rc739a0ikg/deploys/dep-cvfdo5rqf0us73fpipng/{TOKEN}")
-
-    updater.idle()
-
 if __name__ == '__main__':
-    main()
+    import asyncio
+    asyncio.run(main())
